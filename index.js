00b690d165b21df94a586236386e4e5cb7666dcd
ElianEstrada <elian.estrada.urbina@gmail.com>
2022-06-05 15:56:04 -0600

    Agregando documentación al código

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const app = express();
 
+//Crando el servidor
 app.listen(3000, () => {
 
     console.log("Servidor escuchando en el puerto " + 3000);
 
 });
 
+
+//Creando ruta inicial
 app.get('/', (req, res) => {
 
     res.json({"nombre": "Elian", "Edad": 21});
